498940036033be692b64b0d9820f15ddb0f29138
Kyleigh Mason <92942831+KyMason@users.noreply.github.com>
2022-02-24 11:19:25 -0600

    Set up WordGuess template
    Setup how WordGuess will look , adding textboxes in appropriate places

--- MinGameCSharp/MenuForm.Designer.cs
+++ MinGameCSharp/MenuForm.Designer.cs
@@ -38,7 +38,7 @@ namespace MinGameCSharp
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Word Hang";
+            this.button1.Text = "WordGuess";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
---
+++ MinGameCSharp/WordGuess.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace MinGameCSharp
+{
+    partial class WordGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LetterBox1 = new System.Windows.Forms.TextBox();
+            this.letterBox4 = new System.Windows.Forms.TextBox();
+            this.LetterBox2 = new System.Windows.Forms.TextBox();
+            this.letterBox3 = new System.Windows.Forms.TextBox();
+            this.letterBox6 = new System.Windows.Forms.TextBox();
+            this.letterBox5 = new System.Windows.Forms.TextBox();
+            this.letterBox9 = new System.Windows.Forms.TextBox();
+            this.letterBox7 = new System.Windows.Forms.TextBox();
+            this.letterBox8 = new System.Windows.Forms.TextBox();
+            this.incorrLabel1 = new System.Windows.Forms.Label();
+            this.wrongBox1 = new System.Windows.Forms.TextBox();
+            this.wrongBox2 = new System.Windows.Forms.TextBox();
+            this.wrongBox3 = new System.Windows.Forms.TextBox();
+            this.wrongBox4 = new System.Windows.Forms.TextBox();
+            this.wrongBox5 = new System.Windows.Forms.TextBox();
+            this.userInpBox1 = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.infoBox = new System.Windows.Forms.TextBox();
+            this.pointsLabel = new System.Windows.Forms.Label();
+            this.pointTrack = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LetterBox1
+            // 
+            this.LetterBox1.Location = new System.Drawing.Point(12, 209);
+            this.LetterBox1.Name = "LetterBox1";
+            this.LetterBox1.ReadOnly = true;
+            this.LetterBox1.Size = new System.Drawing.Size(80, 27);
+            this.LetterBox1.TabIndex = 0;
+            // 
+            // letterBox4
+            // 
+            this.letterBox4.Location = new System.Drawing.Point(270, 209);
+            this.letterBox4.Name = "letterBox4";
+            this.letterBox4.ReadOnly = true;
+            this.letterBox4.Size = new System.Drawing.Size(80, 27);
+            this.letterBox4.TabIndex = 1;
+            // 
+            // LetterBox2
+            // 
+            this.LetterBox2.Location = new System.Drawing.Point(98, 209);
+            this.LetterBox2.Name = "LetterBox2";
+            this.LetterBox2.ReadOnly = true;
+            this.LetterBox2.Size = new System.Drawing.Size(80, 27);
+            this.LetterBox2.TabIndex = 2;
+            // 
+            // letterBox3
+            // 
+            this.letterBox3.Location = new System.Drawing.Point(184, 209);
+            this.letterBox3.Name = "letterBox3";
+            this.letterBox3.ReadOnly = true;
+            this.letterBox3.Size = new System.Drawing.Size(80, 27);
+            this.letterBox3.TabIndex = 3;
+            // 
+            // letterBox6
+            // 
+            this.letterBox6.Location = new System.Drawing.Point(442, 209);
+            this.letterBox6.Name = "letterBox6";
+            this.letterBox6.ReadOnly = true;
+            this.letterBox6.Size = new System.Drawing.Size(80, 27);
+            this.letterBox6.TabIndex = 4;
+            // 
+            // letterBox5
+            // 
+            this.letterBox5.Location = new System.Drawing.Point(356, 209);
+            this.letterBox5.Name = "letterBox5";
+            this.letterBox5.ReadOnly = true;
+            this.letterBox5.Size = new System.Drawing.Size(80, 27);
+            this.letterBox5.TabIndex = 5;
+            // 
+            // letterBox9
+            // 
+            this.letterBox9.Location = new System.Drawing.Point(700, 209);
+            this.letterBox9.Name = "letterBox9";
+            this.letterBox9.ReadOnly = true;
+            this.letterBox9.Size = new System.Drawing.Size(80, 27);
+            this.letterBox9.TabIndex = 6;
+            // 
+            // letterBox7
+            // 
+            this.letterBox7.Location = new System.Drawing.Point(528, 209);
+            this.letterBox7.Name = "letterBox7";
+            this.letterBox7.ReadOnly = true;
+            this.letterBox7.Size = new System.Drawing.Size(80, 27);
+            this.letterBox7.TabIndex = 7;
+            // 
+            // letterBox8
+            // 
+            this.letterBox8.Location = new System.Drawing.Point(614, 209);
+            this.letterBox8.Name = "letterBox8";
+            this.letterBox8.ReadOnly = true;
+            this.letterBox8.Size = new System.Drawing.Size(80, 27);
+            this.letterBox8.TabIndex = 9;
+            // 
+            // incorrLabel1
+            // 
+            this.incorrLabel1.AutoSize = true;
+            this.incorrLabel1.Location = new System.Drawing.Point(12, 322);
+            this.incorrLabel1.Name = "incorrLabel1";
+            this.incorrLabel1.Size = new System.Drawing.Size(67, 20);
+            this.incorrLabel1.TabIndex = 10;
+            this.incorrLabel1.Text = "Incorrect";
+            // 
+            // wrongBox1
+            // 
+            this.wrongBox1.Location = new System.Drawing.Point(12, 345);
+            this.wrongBox1.Name = "wrongBox1";
+            this.wrongBox1.ReadOnly = true;
+            this.wrongBox1.Size = new System.Drawing.Size(27, 27);
+            this.wrongBox1.TabIndex = 11;
+            // 
+            // wrongBox2
+            // 
+            this.wrongBox2.Location = new System.Drawing.Point(45, 345);
+            this.wrongBox2.Name = "wrongBox2";
+            this.wrongBox2.ReadOnly = true;
+            this.wrongBox2.Size = new System.Drawing.Size(27, 27);
+            this.wrongBox2.TabIndex = 12;
+            // 
+            // wrongBox3
+            // 
+            this.wrongBox3.Location = new System.Drawing.Point(12, 376);
+            this.wrongBox3.Name = "wrongBox3";
+            this.wrongBox3.ReadOnly = true;
+            this.wrongBox3.Size = new System.Drawing.Size(27, 27);
+            this.wrongBox3.TabIndex = 13;
+            // 
+            // wrongBox4
+            // 
+            this.wrongBox4.Location = new System.Drawing.Point(45, 376);
+            this.wrongBox4.Name = "wrongBox4";
+            this.wrongBox4.ReadOnly = true;
+            this.wrongBox4.Size = new System.Drawing.Size(27, 27);
+            this.wrongBox4.TabIndex = 14;
+            // 
+            // wrongBox5
+            // 
+            this.wrongBox5.Location = new System.Drawing.Point(29, 409);
+            this.wrongBox5.Name = "wrongBox5";
+            this.wrongBox5.ReadOnly = true;
+            this.wrongBox5.Size = new System.Drawing.Size(27, 27);
+            this.wrongBox5.TabIndex = 15;
+            // 
+            // userInpBox1
+            // 
+            this.userInpBox1.Location = new System.Drawing.Point(332, 33);
+            this.userInpBox1.Name = "userInpBox1";
+            this.userInpBox1.Size = new System.Drawing.Size(125, 27);
+            this.userInpBox1.TabIndex = 16;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(463, 32);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(94, 29);
+            this.enterButton.TabIndex = 17;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            // 
+            // infoBox
+            // 
+            this.infoBox.Location = new System.Drawing.Point(332, 78);
+            this.infoBox.Multiline = true;
+            this.infoBox.Name = "infoBox";
+            this.infoBox.ReadOnly = true;
+            this.infoBox.Size = new System.Drawing.Size(225, 74);
+            this.infoBox.TabIndex = 18;
+            this.infoBox.Text = "Enter a letter above.";
+            this.infoBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Location = new System.Drawing.Point(700, 345);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(51, 20);
+            this.pointsLabel.TabIndex = 19;
+            this.pointsLabel.Text = "Points:";
+            // 
+            // pointTrack
+            // 
+            this.pointTrack.AutoSize = true;
+            this.pointTrack.Location = new System.Drawing.Point(700, 376);
+            this.pointTrack.MinimumSize = new System.Drawing.Size(51, 0);
+            this.pointTrack.Name = "pointTrack";
+            this.pointTrack.Size = new System.Drawing.Size(51, 20);
+            this.pointTrack.TabIndex = 20;
+            this.pointTrack.Text = "0";
+            this.pointTrack.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // WordGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pointTrack);
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.infoBox);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.userInpBox1);
+            this.Controls.Add(this.wrongBox5);
+            this.Controls.Add(this.wrongBox4);
+            this.Controls.Add(this.wrongBox3);
+            this.Controls.Add(this.wrongBox2);
+            this.Controls.Add(this.wrongBox1);
+            this.Controls.Add(this.incorrLabel1);
+            this.Controls.Add(this.letterBox8);
+            this.Controls.Add(this.letterBox7);
+            this.Controls.Add(this.letterBox9);
+            this.Controls.Add(this.letterBox5);
+            this.Controls.Add(this.letterBox6);
+            this.Controls.Add(this.letterBox3);
+            this.Controls.Add(this.LetterBox2);
+            this.Controls.Add(this.letterBox4);
+            this.Controls.Add(this.LetterBox1);
+            this.Name = "WordGuess";
+            this.Text = "Enter Guess";
+            this.Load += new System.EventHandler(this.WordHang_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox LetterBox1;
+        private System.Windows.Forms.TextBox letterBox4;
+        private System.Windows.Forms.TextBox LetterBox2;
+        private System.Windows.Forms.TextBox letterBox3;
+        private System.Windows.Forms.TextBox letterBox6;
+        private System.Windows.Forms.TextBox letterBox5;
+        private System.Windows.Forms.TextBox letterBox9;
+        private System.Windows.Forms.TextBox letterBox7;
+        private System.Windows.Forms.TextBox letterBox8;
+        private System.Windows.Forms.Label incorrLabel1;
+        private System.Windows.Forms.TextBox wrongBox1;
+        private System.Windows.Forms.TextBox wrongBox2;
+        private System.Windows.Forms.TextBox wrongBox3;
+        private System.Windows.Forms.TextBox wrongBox4;
+        private System.Windows.Forms.TextBox wrongBox5;
+        private System.Windows.Forms.TextBox userInpBox1;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.TextBox infoBox;
+        private System.Windows.Forms.Label pointsLabel;
+        private System.Windows.Forms.Label pointTrack;
+    }
+}
